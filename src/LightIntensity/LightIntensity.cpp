fe80b2012e29481d7d47fc0ce31fd3498ea38e7f
WitoldPietrzak <60006320+WitoldPietrzak@users.noreply.github.com>
2022-03-23 11:10:30 +0100

    Started working on light intensity

---
+++ src/LightIntensity/LightIntensity.cpp
@@ -0,0 +1,101 @@
+//
+// Created by Witek on 22.03.2022.
+//
+
+#include "LightIntensity.h"
+
+LightIntensity::LightIntensity(int r, int g, int b) : r(r), g(g), b(b) {}
+
+int LightIntensity::getR() const {
+    return r;
+}
+
+void LightIntensity::setR(int r) {
+    LightIntensity::r = r;
+}
+
+int LightIntensity::getG() const {
+    return g;
+}
+
+void LightIntensity::setG(int g) {
+    LightIntensity::g = g;
+}
+
+int LightIntensity::getB() const {
+    return b;
+}
+
+void LightIntensity::setB(int b) {
+    LightIntensity::b = b;
+}
+
+void LightIntensity::add(int R, int G, int B) {
+    this->setR(this->getR() + R);
+    this->setG(this->getG() + G);
+    this->setB(this->getB() + B);
+}
+
+LightIntensity LightIntensity::add(LightIntensity &li) {
+    this->setR(this->getR() + li.getR());
+    this->setG(this->getG() + li.getG());
+    this->setB(this->getB() + li.getB());
+}
+
+LightIntensity LightIntensity::BLACK() {
+    return {0, 0, 0};
+}
+
+LightIntensity LightIntensity::WHITE() {
+    return {255, 255, 255};
+}
+
+LightIntensity LightIntensity::RED() {
+    return {255, 0, 0};
+}
+
+LightIntensity LightIntensity::GREEN() {
+    return {0, 255, 0};
+}
+
+LightIntensity LightIntensity::BLUE() {
+    return {0, 0, 255};
+}
+
+LightIntensity LightIntensity::sum(LightIntensity &li) {
+    return {this->getR() + li.getR(), this->getG() + li.getG(), this->getB() + li.getB()};
+}
+
+LightIntensity LightIntensity::sum(int num) {
+    return {this->getR() + num, this->getG() + num, this->getB() + num};
+}
+
+LightIntensity LightIntensity::sub(LightIntensity &li) {
+    return {this->getR() - li.getR(), this->getG() - li.getG(), this->getB() - li.getB()};
+}
+
+LightIntensity LightIntensity::sub(int num) {
+    return {this->getR() - num, this->getG() - num, this->getB() - num};
+}
+
+LightIntensity LightIntensity::operator+(LightIntensity &li) {
+    return sum(li);
+}
+
+LightIntensity LightIntensity::operator-(LightIntensity &li) {
+    return sub(li);
+}
+
+LightIntensity LightIntensity::operator+(int num) {
+    return sum(num);
+}
+
+LightIntensity LightIntensity::operator-(int num) {
+    return sub(num);
+}
+
+void LightIntensity::operator+=(LightIntensity &li) {
+    add(li);
+}
+
+
---
+++ src/LightIntensity/LightIntensity.h
@@ -0,0 +1,79 @@
+//
+// Created by Witek on 22.03.2022.
+//
+
+#ifndef FOTO_LIGHTINTENSITY_H
+#define FOTO_LIGHTINTENSITY_H
+
+
+class LightIntensity {
+private:
+    int r, g, b;
+public:
+    LightIntensity(double r, double g, double b);
+
+    LightIntensity(int r, int g, int b);
+
+    int getR() const;
+
+    void setR(int r);
+
+    int getG() const;
+
+    void setG(int g);
+
+    int getB() const;
+
+    void setB(int b);
+
+    void add(int R, int G, int B);
+
+    LightIntensity add(LightIntensity &li);
+
+    LightIntensity sum(LightIntensity &li);
+
+    LightIntensity sum(int num);
+
+    LightIntensity sub(LightIntensity &li);
+
+    LightIntensity sub(int sum);
+
+    LightIntensity div(float num);
+
+    LightIntensity multiply(float num);
+
+
+    static LightIntensity BLACK();
+
+    static LightIntensity WHITE();
+
+    static LightIntensity RED();
+
+    static LightIntensity GREEN();
+
+    static LightIntensity BLUE();
+
+    LightIntensity operator+(LightIntensity &li);
+
+    LightIntensity operator+(int num);
+
+    LightIntensity operator-(LightIntensity &li);
+
+    LightIntensity operator-(int num);
+
+    LightIntensity operator/(float num);
+
+    void operator+=(LightIntensity &li);
+
+    LightIntensity operator-=(LightIntensity &li);
+
+    LightIntensity operator*=(float num);
+
+    LightIntensity operator/=(float num);
+
+    friend LightIntensity operator*(float num, LightIntensity &li);
+
+    friend LightIntensity operator*(LightIntensity &li, float num);
+};
+
+#endif //FOTO_LIGHTINTENSITY_H
