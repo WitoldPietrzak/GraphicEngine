07361e463aab4a2ba9b6dfa383b83e23986a3a02
WitoldP99 <witekpietrzak35@gmail.com>
2022-03-31 20:12:14 +0200

    Start working on Triangle

--- src/Structures/Triangle/Triangle.cpp
+++ src/Structures/Triangle/Triangle.cpp
@@ -7,3 +7,41 @@
 std::vector<Vector3> Triangle::intersections(Ray ray) const {
     return std::vector<Vector3>();
 }
+
+const Vector3 &Triangle::getA() const {
+    return a;
+}
+
+void Triangle::setA(const Vector3 &a) {
+    Triangle::a = a;
+}
+
+const Vector3 &Triangle::getB() const {
+    return b;
+}
+
+void Triangle::setB(const Vector3 &b) {
+    Triangle::b = b;
+}
+
+const Vector3 &Triangle::getC() const {
+    return c;
+}
+
+void Triangle::setC(const Vector3 &c) {
+    Triangle::c = c;
+}
+
+const Plane &Triangle::getPlane() const {
+    return plane;
+}
+
+void Triangle::setPlane(const Plane &plane) {
+    Triangle::plane = plane;
+}
+
+Triangle::Triangle(const LightIntensity &color, const Vector3 &a, const Vector3 &b, const Vector3 &c,
+                   const Plane &plane) : Structure(color), a(a), b(b), c(c), plane(plane) {}
+
+Triangle::Triangle(const Vector3 &a, const Vector3 &b, const Vector3 &c, const Plane &plane) : a(a), b(b), c(c),
+                                                                                               plane(plane) {}
--- src/Structures/Triangle/Triangle.h
+++ src/Structures/Triangle/Triangle.h
@@ -7,12 +7,35 @@
 
 
 #include "../Structure/Structure.h"
+#include "../Plane/Plane.h"
 
 class Triangle: public Structure {
 private:
     Vector3 a,b,c;
+    Plane plane;
 public:
+    Triangle(const LightIntensity &color, const Vector3 &a, const Vector3 &b, const Vector3 &c, const Plane &plane);
+
+    Triangle(const Vector3 &a, const Vector3 &b, const Vector3 &c, const Plane &plane);
+
     std::vector<Vector3> intersections(Ray ray) const override;
+
+    const Vector3 &getA() const;
+
+    void setA(const Vector3 &a);
+
+    const Vector3 &getB() const;
+
+    void setB(const Vector3 &b);
+
+    const Vector3 &getC() const;
+
+    void setC(const Vector3 &c);
+
+    const Plane &getPlane() const;
+
+    void setPlane(const Plane &plane);
+
 
 };
 
