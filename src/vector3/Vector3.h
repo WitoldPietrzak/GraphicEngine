3a73fbbe335927e2bbdebdb24f21f7c11e086e97
WitoldP99 <HrRLN7jZ7sLva9VPaauh>
2022-05-06 23:56:23 +0200

    Update Vector3.h

--- src/vector3/Vector3.h
+++ src/vector3/Vector3.h
@@ -8,12 +8,10 @@
 
 #include <string>
 
-class Vector3{
+class Vector3 {
 
 private:
-    float x;
-    float y;
-    float z;
+    float x, y, z;
 public:
     float getX() const;
 
@@ -27,45 +25,76 @@ public:
 
     void setZ(float z);
 
-    float  getLength() const;
+    float getLength() const;
 
     float getLengthSquared() const;
 
     Vector3(float x, float y, float z);
-    Vector3(const Vector3& v1, const Vector3& v2);
-    Vector3(Vector3 const &vector3);
 
-    Vector3 sum(const Vector3& obj);
-    Vector3 sub(const Vector3& obj) const;
+    Vector3(const Vector3 &v1, const Vector3 &v2);
+
+    Vector3(const Vector3 &vector3);
+
+    Vector3 sum(const Vector3 &obj);
+
+    Vector3 sub(const Vector3 &obj) const;
+
     Vector3 multiply(float k);
-    Vector3 multiply(const Vector3& vector3);
-    Vector3 multiplyVector(const Vector3& vector3) const;
-    float multiplyScalar( Vector3 vector3) const;
+
+    Vector3 multiply(const Vector3 &vector3);
+
+    Vector3 multiplyVector(const Vector3 &vector3) const;
+
+    float multiplyScalar(Vector3 vector3) const;
+
     Vector3 div(float k);
-    Vector3 div(const Vector3& vector3);
+
+    Vector3 div(const Vector3 &vector3);
+
+
     void normalize();
+
     Vector3 getNormalized();
 
-    Vector3 operator + ();
-    Vector3 operator - ();
-    Vector3 operator + (const Vector3& obj);
-    Vector3 operator - (const Vector3& obj) const;
-    Vector3 operator - ( Vector3 obj);
-    Vector3 operator * (const Vector3& obj);
-    Vector3 operator * (float k);
-    Vector3 operator / (const Vector3& obj);
-    Vector3 operator / (float k);
+    static Vector3 bisectingVector(Vector3 vector1, Vector3 vector2);
 
-    void operator += (const Vector3& obj);
-    void operator -= (const Vector3& obj);
-    void operator *= (const Vector3& obj);
-    void operator /= (const Vector3& obj);
-    bool operator == (const Vector3& obj);
-    bool operator != (const Vector3& obj);
-    void operator *= (float k);
-    void operator /= (float k);
+    Vector3 rotateVector(Vector3 rotationVector, float number);
 
+    Vector3 rotatePoint(Vector3 center, Vector3 rotationVector, float number);
 
+    Vector3 operator+();
+
+    Vector3 operator-();
+
+    Vector3 operator+(const Vector3 &obj);
+
+    Vector3 operator-(const Vector3 &obj) const;
+
+    Vector3 operator-(Vector3 obj);
+
+    Vector3 operator*(const Vector3 &obj);
+
+    Vector3 operator*(float k);
+
+    Vector3 operator/(const Vector3 &obj);
+
+    Vector3 operator/(float k);
+
+    void operator+=(const Vector3 &obj);
+
+    void operator-=(const Vector3 &obj);
+
+    void operator*=(const Vector3 &obj);
+
+    void operator/=(const Vector3 &obj);
+
+    bool operator==(const Vector3 &obj);
+
+    bool operator!=(const Vector3 &obj);
+
+    void operator*=(float k);
+
+    void operator/=(float k);
 
 
 };
