7c13c2647de906c675c9acbbaaa04e60b037c602
WitoldP99 <HrRLN7jZ7sLva9VPaauh>
2022-05-06 23:58:16 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -27,165 +27,167 @@ void readVectorList(const std::vector<Vector3> &vectors) {
     }
 }
 
-void task1() {
-    Vector3 sphereVector = Vector3(0, 0, 0);
-    Vector3 rayVector = Vector3(0, 0, -20);
+//void task1() {
+//    Vector3 sphereVector = Vector3(0, 0, 0);
+//    Vector3 rayVector = Vector3(0, 0, -20);
+//
+//    //2 Zdefiniować sferę S o środku w punkcie (0,0,0) i promieniu 10.
+//    Sphere S1 = Sphere(sphereVector, 10);
+//    //3 Zdefiniować promień R1 o początku w punkcie (0,0,-20) i skierowany w środek kuli.
+//    Ray R1 = Ray(rayVector, Vector3(rayVector, sphereVector).getNormalized());
+//    //4 Zdefiniować promień R2 o początku w tym samym punkcie, co R1, skierowany równolegle do osi Y.
+//    Ray R2 = Ray(rayVector, Vector3(0, 1, 0).getNormalized());
+//    //5  Sprawdzić, czy istnieje przecięcie sfery S z promieniami R1 oraz R2
+//    auto I1 = S1.intersections(R1);
+//    auto I2 = S1.intersections(R2);
+//    std::cout << "5: \n";
+//    std::cout << "R1: \n";
+//    readVectorList(I1);
+//    std::cout << "R2: \n";
+//    readVectorList(I2);
+//
+//    //6 Proszę zdefiniować dowolny promień R3, tak aby przecinał on sferę S w dokładnie jednym punkcie. Podać współrzędne punktu przecięcia
+//    Ray R3 = Ray(Vector3(0, 50, 10), Vector3(0, -1, 0).getNormalized());
+//    auto I3 = S1.intersections(R3);
+//    std::cout << "6: \n";
+//    std::cout << "R3: \n";
+//    readVectorList(I3);
+//
+//    //7 Proszę zdefiniować płaszczyznę P przechodzącą przez punkt (0,0,0), której wektor normalny tworzy kąt 45 stopni z osiami Y i Z.
+//    Plane P1 = Plane(Vector3(0, 0.5, 0.5), Vector3(0, 0, 0));
+//
+//    //8 Proszę znaleźć punkt przecięcia płaszczyzny P z promieniem R2.
+//    std::cout << "8: \n";
+//    std::cout << "R2: \n";
+//    auto I4 = P1.intersections(R2);
+//    readVectorList(I4);
+//
+//
+//    // Dodatkowe testy
+//
+//    // Nieskonczone przeciecie
+//    std::cout << "Infinite intersections test: \n";
+//    auto P2 = Plane(Vector3(0, 1, 0), Vector3(0, 0, 0));
+//    auto R4 = Ray(Vector3(0, 0, 0), Vector3(0, 0, 1));
+//    try {
+//        auto I5 = P2.intersections(R4);
+//    } catch (InfiniteIntersectionException &exception) {
+//        std::cout << "Infinite intersections exception occured \n";
+//    }
+//
+//    // Promien z poczatkiem w srodku S1
+//    auto R5 = Ray(Vector3(0, 0, 0), Vector3(1, 0, 0));
+//    auto I6 = S1.intersections(R5);
+//    std::cout << "Ray from inside S1: \n";
+//    readVectorList(I6);
+//}
 
-    //2 Zdefiniować sferę S o środku w punkcie (0,0,0) i promieniu 10.
-    Sphere S1 = Sphere(sphereVector, 10);
-    //3 Zdefiniować promień R1 o początku w punkcie (0,0,-20) i skierowany w środek kuli.
-    Ray R1 = Ray(rayVector, Vector3(rayVector, sphereVector).getNormalized());
-    //4 Zdefiniować promień R2 o początku w tym samym punkcie, co R1, skierowany równolegle do osi Y.
-    Ray R2 = Ray(rayVector, Vector3(0, 1, 0).getNormalized());
-    //5  Sprawdzić, czy istnieje przecięcie sfery S z promieniami R1 oraz R2
-    auto I1 = S1.intersections(R1);
-    auto I2 = S1.intersections(R2);
-    std::cout << "5: \n";
-    std::cout << "R1: \n";
-    readVectorList(I1);
-    std::cout << "R2: \n";
-    readVectorList(I2);
-
-    //6 Proszę zdefiniować dowolny promień R3, tak aby przecinał on sferę S w dokładnie jednym punkcie. Podać współrzędne punktu przecięcia
-    Ray R3 = Ray(Vector3(0, 50, 10), Vector3(0, -1, 0).getNormalized());
-    auto I3 = S1.intersections(R3);
-    std::cout << "6: \n";
-    std::cout << "R3: \n";
-    readVectorList(I3);
-
-    //7 Proszę zdefiniować płaszczyznę P przechodzącą przez punkt (0,0,0), której wektor normalny tworzy kąt 45 stopni z osiami Y i Z.
-    Plane P1 = Plane(Vector3(0, 0.5, 0.5), Vector3(0, 0, 0));
-
-    //8 Proszę znaleźć punkt przecięcia płaszczyzny P z promieniem R2.
-    std::cout << "8: \n";
-    std::cout << "R2: \n";
-    auto I4 = P1.intersections(R2);
-    readVectorList(I4);
-
-
-    // Dodatkowe testy
-
-    // Nieskonczone przeciecie
-    std::cout << "Infinite intersections test: \n";
-    auto P2 = Plane(Vector3(0, 1, 0), Vector3(0, 0, 0));
-    auto R4 = Ray(Vector3(0, 0, 0), Vector3(0, 0, 1));
-    try {
-        auto I5 = P2.intersections(R4);
-    } catch (InfiniteIntersectionException &exception) {
-        std::cout << "Infinite intersections exception occured \n";
-    }
-
-    // Promien z poczatkiem w srodku S1
-    auto R5 = Ray(Vector3(0, 0, 0), Vector3(1, 0, 0));
-    auto I6 = S1.intersections(R5);
-    std::cout << "Ray from inside S1: \n";
-    readVectorList(I6);
-}
-
-void task2() {
-    Structure *sphere = new Sphere(Vector3(0, 0, 0), 80, LightIntensity::BLUE());
-    Structure *sphere2 = new Sphere(Vector3(-120, 0, -200), 80, LightIntensity::RED());
-
-    OrthogonalCamera ort = OrthogonalCamera(Vector3(0, 0, 400), Vector3(0, 0, -1), Vector3(0, 1, 0), 600, 600,
-                                            OrthogonalSampler(4));
-    PerspectiveCamera perspectiveCamera = PerspectiveCamera(Vector3(0, 0, 400), Vector3(0, 0, -1), Vector3(0, 1, 0),
-                                                            400, 400, 400, PerspectiveSampler(4));
-    Scene scene = Scene();
-    scene.setBackgroundColor(LightIntensity::BLACK());
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 6; j++) {
-            LightIntensity color;
-            if(j == 5){
-                color = LightIntensity(51*i,0,0);
-            }
-            if(j == 4){
-                color = LightIntensity(0,51*i,0);
-            }
-            if(j == 3){
-                color = LightIntensity(0,0,51*i);
-            }
-            if(j == 2){
-                color = LightIntensity(255,0,51*i);
-            }
-            if(j == 1){
-                color = LightIntensity(0,255,51*i);
-            }
-            if(j == 0){
-                color = LightIntensity(255,255,51*i);
-            }
-            scene.addStructure(new LimitedPlane(Vector3(0, 0, -1), Vector3(0, 0, -200), color,
-                                                Vector3(-300 + (j * 100), 300 - (i * 100), -200),
-                                                Vector3(-200 + (j * 100), 300 - (i * 100) , -200),
-                                                Vector3(-300 + (j * 100), 200 - (i * 100), -200),
-                                                Vector3(-200 + (j * 100), 200 - (i * 100), -200)));
-        }
-    }
-    scene.addStructure(sphere);
-    scene.addStructure(sphere2);
-    auto test2 = ort.renderScene(scene, 500, 500);
-    auto test3 = perspectiveCamera.renderScene(scene,500,500);
-    test2.save("orthogonal.bmp");
-    test3.save("perspective.bmp");
-
-
-
-}
-
-void task3_test() {
-    Structure *sphere = new Sphere(Vector3(0, 0, 0), 80, LightIntensity::BLUE());
-    Structure *sphere2 = new Sphere(Vector3(-120, 0, -200), 80, LightIntensity::RED());
-    Structure *triangle = new Triangle(Vector3(-50,-50,0),Vector3(50,-50,0),Vector3(0,50,0),LightIntensity::WHITE());
-    Structure *triangle2 = new Triangle(Vector3(0,-80,0),Vector3(90,-100,0),Vector3(50,20,0),LightIntensity::RED());
-
-    OrthogonalCamera ort = OrthogonalCamera(Vector3(0, 0, -400), Vector3(0, 0, 1), Vector3(0, 1, 0), 600, 600,
-                                            OrthogonalSampler(4));
-    PerspectiveCamera perspectiveCamera = PerspectiveCamera(Vector3(0, 0, -400), Vector3(0, 0, 1), Vector3(0, 1, 0),
-                                                            400, 400, 400, PerspectiveSampler(4));
-    Scene scene = Scene();
-    scene.setBackgroundColor(LightIntensity::BLACK());
+//void task2() {
+//    Structure *sphere = new Sphere(Vector3(0, 0, 0), 80, LightIntensity::BLUE());
+//    Structure *sphere2 = new Sphere(Vector3(-120, 0, -200), 80, LightIntensity::RED());
+//
+//    OrthogonalCamera ort = OrthogonalCamera(Vector3(0, 0, 400), Vector3(0, 0, -1), Vector3(0, 1, 0), 600, 600,
+//                                            OrthogonalSampler(4));
+//    PerspectiveCamera perspectiveCamera = PerspectiveCamera(Vector3(0, 0, 400), Vector3(0, 0, -1), Vector3(0, 1, 0),
+//                                                            400, 400, 400, PerspectiveSampler(4));
+//    Scene scene = Scene();
+//    scene.setBackgroundColor(LightIntensity::BLACK());
+//    for (int i = 0; i < 6; i++) {
+//        for (int j = 0; j < 6; j++) {
+//            LightIntensity color;
+//            if(j == 5){
+//                color = LightIntensity(51*i,0,0);
+//            }
+//            if(j == 4){
+//                color = LightIntensity(0,51*i,0);
+//            }
+//            if(j == 3){
+//                color = LightIntensity(0,0,51*i);
+//            }
+//            if(j == 2){
+//                color = LightIntensity(255,0,51*i);
+//            }
+//            if(j == 1){
+//                color = LightIntensity(0,255,51*i);
+//            }
+//            if(j == 0){
+//                color = LightIntensity(255,255,51*i);
+//            }
+//            scene.addStructure(new LimitedPlane(Vector3(0, 0, -1), Vector3(0, 0, -200), color,
+//                                                Vector3(-300 + (j * 100), 300 - (i * 100), -200),
+//                                                Vector3(-200 + (j * 100), 300 - (i * 100) , -200),
+//                                                Vector3(-300 + (j * 100), 200 - (i * 100), -200),
+//                                                Vector3(-200 + (j * 100), 200 - (i * 100), -200)));
+//        }
+//    }
 //    scene.addStructure(sphere);
 //    scene.addStructure(sphere2);
-    scene.addStructure(triangle);
-    scene.addStructure(triangle2);
-    auto test2 = ort.renderScene(scene, 500, 500);
-    auto test3 = perspectiveCamera.renderScene(scene,500,500);
-    test2.save("orthogonal-triangle.bmp");
-    test3.save("perspective-triangle.bmp");
+//    auto test2 = ort.renderScene(scene, 500, 500);
+//    auto test3 = perspectiveCamera.renderScene(scene,500,500);
+//    test2.save("orthogonal.bmp");
+//    test3.save("perspective.bmp");
+//
+//
+//
+//}
 
+//void task3_test() {
+//    Structure *sphere = new Sphere(Vector3(0, 0, 0), 80, LightIntensity::BLUE());
+//    Structure *sphere2 = new Sphere(Vector3(-120, 0, -200), 80, LightIntensity::RED());
+//    Structure *triangle = new Triangle(Vector3(-50,-50,0),Vector3(50,-50,0),Vector3(0,50,0),LightIntensity::WHITE());
+//    Structure *triangle2 = new Triangle(Vector3(0,-80,0),Vector3(90,-100,0),Vector3(50,20,0),LightIntensity::RED());
+//
+//    OrthogonalCamera ort = OrthogonalCamera(Vector3(0, 0, -400), Vector3(0, 0, 1), Vector3(0, 1, 0), 600, 600,
+//                                            OrthogonalSampler(4));
+//    PerspectiveCamera perspectiveCamera = PerspectiveCamera(Vector3(0, 0, -400), Vector3(0, 0, 1), Vector3(0, 1, 0),
+//                                                            400, 400, 400, PerspectiveSampler(4));
+//    Scene scene = Scene();
+//    scene.setBackgroundColor(LightIntensity::BLACK());
+////    scene.addStructure(sphere);
+////    scene.addStructure(sphere2);
+//    scene.addStructure(triangle);
+//    scene.addStructure(triangle2);
+//    auto test2 = ort.renderScene(scene, 500, 500);
+//    auto test3 = perspectiveCamera.renderScene(scene,500,500);
+//    test2.save("orthogonal-triangle.bmp");
+//    test3.save("perspective-triangle.bmp");
+//
+//
+//
+//}
 
-
-}
-
-void task3() {
-
-    ObjParser parser = ObjParser();
-    Structure *mesh = parser.parse("object.obj");
-
-    OrthogonalCamera ort = OrthogonalCamera(Vector3(0, 0, -4), Vector3(0, 0, 1), Vector3(0, 1, 0), 4, 4,
-                                            OrthogonalSampler(4));
-    PerspectiveCamera perspectiveCamera = PerspectiveCamera(Vector3(0, 0, -4), Vector3(0, 0, 1), Vector3(0, 1, 0),
-                                                            4, 4, 4, PerspectiveSampler(4));
-    PerspectiveCamera perspectiveCamera2 = PerspectiveCamera(Vector3(-sqrtf(2), 2, -sqrtf(2)), Vector3(1, -1, 1).getNormalized(), Vector3(0, 1, 0),
-                                                            4, 4, 4, PerspectiveSampler(4));
-    Scene scene = Scene();
-    mesh->setColor(LightIntensity::RED());
-    scene.addStructure(mesh);
-    scene.setBackgroundColor(LightIntensity::BLACK());
-    auto test2 = ort.renderScene(scene, 500, 500);
-    auto test3 = perspectiveCamera.renderScene(scene,500,500);
-    auto test4 = perspectiveCamera2.renderScene(scene,500,500);
-    test2.save("orthogonal-mesh.bmp");
-    test3.save("perspective-mesh.bmp");
-    test4.save("perspective-mesh-angle.bmp");
-
-
-
-}
+//void task3() {
+//
+//    ObjParser parser = ObjParser();
+//    Structure *mesh = parser.parse("object.obj");
+//
+//    OrthogonalCamera ort = OrthogonalCamera(Vector3(0, 0, -4), Vector3(0, 0, 1), Vector3(0, 1, 0), 4, 4,
+//                                            OrthogonalSampler(4));
+//    PerspectiveCamera perspectiveCamera = PerspectiveCamera(Vector3(0, 0, -4), Vector3(0, 0, 1), Vector3(0, 1, 0),
+//                                                            4, 4, 4, PerspectiveSampler(4));
+//    PerspectiveCamera perspectiveCamera2 = PerspectiveCamera(Vector3(-sqrtf(2), 2, -sqrtf(2)), Vector3(1, -1, 1).getNormalized(), Vector3(0, 1, 0),
+//                                                            4, 4, 4, PerspectiveSampler(4));
+//    Scene scene = Scene();
+//    mesh->setColor(LightIntensity::RED());
+//    scene.addStructure(mesh);
+//    scene.setBackgroundColor(LightIntensity::BLACK());
+//    auto test2 = ort.renderScene(scene, 500, 500);
+//    auto test3 = perspectiveCamera.renderScene(scene,500,500);
+//    auto test4 = perspectiveCamera2.renderScene(scene,500,500);
+//    test2.save("orthogonal-mesh.bmp");
+//    test3.save("perspective-mesh.bmp");
+//    test4.save("perspective-mesh-angle.bmp");
+//
+//
+//
+//}
 
 void task3(std::string name) {
 
     ObjParser parser = ObjParser();
     Structure *mesh = parser.parse(name);
+    Structure *sphere = new Sphere(Vector3(-0.75,0.5,0.5),0.2,LightIntensity::RED());
+    auto *light = new PointLight(LightIntensity(250,25,250), Vector3(0.5,0.5,-4));
 
     OrthogonalCamera ort = OrthogonalCamera(Vector3(0, 0, -4), Vector3(0, 0, 1), Vector3(0, 1, 0), 4, 4,
                                             OrthogonalSampler(4));
@@ -193,16 +195,24 @@ void task3(std::string name) {
                                                             4, 4, 4, PerspectiveSampler(4));
     PerspectiveCamera perspectiveCamera2 = PerspectiveCamera(Vector3(-sqrtf(2), 2, -sqrtf(2)), Vector3(1, -1, 1).getNormalized(), Vector3(0, 1, 0),
                                                              4, 4, 4, PerspectiveSampler(4));
+
+    OrthogonalCamera orthogonalCamera = OrthogonalCamera(Vector3(-sqrtf(4), 2, -sqrtf(4)), Vector3(1, -1, 1).getNormalized(), Vector3(0, 1, 0), 4, 4,
+                                                         OrthogonalSampler(4));
     Scene scene = Scene();
-    mesh->setColor(LightIntensity::RED());
+    mesh->setColor(LightIntensity::BLUE());
     scene.addStructure(mesh);
+    scene.addStructure(sphere);
     scene.setBackgroundColor(LightIntensity::BLACK());
+    scene.setAmbient(LightIntensity(88,255,145));
+    scene.addLightSource(light);
     auto test2 = ort.renderScene(scene, 500, 500);
-    auto test3 = perspectiveCamera.renderScene(scene,500,500);
-    auto test4 = perspectiveCamera2.renderScene(scene,500,500);
+//    auto test3 = perspectiveCamera.renderScene(scene,500,500);
+//    auto test4 = perspectiveCamera2.renderScene(scene,500,500);
+//    auto test5 = orthogonalCamera.renderScene(scene,500,500);
     test2.save("orthogonal-custom.bmp");
-    test3.save("perspective-custom.bmp");
-    test4.save("perspective-custom-angle.bmp");
+//    test5.save("orthogonal-custom-angle.bmp");
+//    test3.save("perspective-custom.bmp");
+//    test4.save("perspective-custom-angle.bmp");
 
 
 
@@ -223,12 +233,12 @@ void task3_teapot() {
     mesh->setColor(LightIntensity::RED());
     scene.addStructure(mesh);
     scene.setBackgroundColor(LightIntensity::BLACK());
-    auto test2 = ort.renderScene(scene, 256, 256);
-//    auto test3 = perspectiveCamera.renderScene(scene,256,256);
-//    auto test4 = perspectiveCamera2.renderScene(scene,256,256);
-    test2.save("orthogonal-teapot.bmp");
-//    test3.save("perspective-teapot.bmp");
-//    test4.save("perspective-teapot-angle.bmp");
+//    auto test2 = ort.renderScene(scene, 256, 256);
+    auto test3 = perspectiveCamera.renderScene(scene,256,256);
+    auto test4 = perspectiveCamera2.renderScene(scene,256,256);
+//    test2.save("orthogonal-teapot.bmp");
+    test3.save("perspective-teapot.bmp");
+    test4.save("perspective-teapot-angle.bmp");
 
 
 
