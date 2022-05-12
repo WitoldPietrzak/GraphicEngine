f32a90be6dbead2a21bda5b26d42395c194a2031
WitoldPietrzak <60006320+WitoldPietrzak@users.noreply.github.com>
2022-05-12 16:33:51 +0200

    Start work on textures

---
+++ src/Texture/Texture.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Witek on 10.05.2022.
+//
+
+#include "Texture.h"
---
+++ src/Texture/Texture.h
@@ -0,0 +1,21 @@
+//
+// Created by Witek on 10.05.2022.
+//
+
+#ifndef FOTO_TEXTURE_H
+#define FOTO_TEXTURE_H
+
+
+#include "../LightIntensity/LightIntensity.h"
+
+class Texture {
+private:
+    int width;
+    int height;
+    LightIntensity colorMap[];
+public:
+
+};
+
+
+#endif //FOTO_TEXTURE_H
